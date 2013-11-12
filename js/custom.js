19770b661655cca97e1b3df99e59fd3fa09a9ac2
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2013-11-12 15:32:01 -0200

    add página do curso de rails

--- js/custom.js
+++ js/custom.js
@@ -16,3 +16,26 @@ $(function() {
 $(function () {
     $.stellar();
 });
+
+
+/* --------------------- Tabs ------------------------ */
+
+$(function() {
+    var $tabsNav    = $('.tabs-nav'),
+        $tabsNavLis = $tabsNav.children('li'),
+        $tabContent = $('.tab-content');
+
+    $tabsNav.each(function() {
+        var $this = $(this);
+        $this.next().children('.tab-content').stop(true,true).hide().first().show();
+        $this.children('li').first().addClass('active').stop(true,true).show();
+    });
+
+    $tabsNavLis.on('click', function(e) {
+        var $this = $(this);
+        $this.siblings().removeClass('active').end().addClass('active');
+        $this.parent().next().children('.tab-content').stop(true,true)
+             .hide().siblings( $this.find('a').attr('href') ).fadeIn();
+        e.preventDefault();
+    });
+});
